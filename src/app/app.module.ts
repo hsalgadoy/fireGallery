f298a34065c84ff782f063bfadcef75b07606289
hsalgadoy <52671523+hsalgadoy@users.noreply.github.com>
2019-10-08 16:59:48 -0500

    firegalleryInitial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// 2. Add your credentials from step 1
+const firebaseConfig = {
+  apiKey: "AIzaSyBCHmL2qgth1SC8mX9IENJP-cDhXYiM7OM",
+  authDomain: "firegallery-448d4.firebaseapp.com",
+  databaseURL: "https://firegallery-448d4.firebaseio.com",
+  projectId: "firegallery-448d4",
+  storageBucket: "",
+  messagingSenderId: "148670081419",
+  appId: "1:148670081419:web:5a93ba794fa1ba91fb278e"
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // 3. Initialize
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/notification-services/notification-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-services',
+  templateUrl: './notification-services.component.html',
+  styleUrls: ['./notification-services.component.css']
+})
+export class NotificationServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
